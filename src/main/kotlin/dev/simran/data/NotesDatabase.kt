458f3cb48ac093d0fdafedc03f296dbee41efe24
Simran <simran5590@gmail.com>
2021-05-20 08:02:40 -0700

    Moved connection string into a constant

--- src/main/kotlin/dev/simran/data/NotesDatabase.kt
+++ src/main/kotlin/dev/simran/data/NotesDatabase.kt
@@ -6,8 +6,11 @@ import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.eq
 import org.litote.kmongo.reactivestreams.KMongo
 
+
 private const val DATABASE_NAME = "NotesDatabase"
-private val client = KMongo.createClient("mongodb+srv://adminuser:adminuser@cluster0.mg0dr.mongodb.net/$DATABASE_NAME?retryWrites=true&w=majority").coroutine
+private const val CONNECTION_STRING = "mongodb+srv://adminuser:adminuser@cluster0.mg0dr.mongodb.net/$DATABASE_NAME?retryWrites=true&w=majority"
+
+private val client = KMongo.createClient(CONNECTION_STRING).coroutine
 private val database = client.getDatabase(DATABASE_NAME)
 private val users = database.getCollection<User>()
 private val notes = database.getCollection<Note>()
