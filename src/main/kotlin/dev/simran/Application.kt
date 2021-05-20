b1a043413e049aba0af51b47d09cd07b62804bfa
Simran <simran5590@gmail.com>
2021-05-19 20:34:50 -0700

    Basic configurations setup

--- src/main/kotlin/dev/simran/Application.kt
+++ src/main/kotlin/dev/simran/Application.kt
@@ -3,9 +3,27 @@ package dev.simran
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import dev.simran.plugins.*
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
+import io.ktor.routing.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        // Setting defaults headers
+        install(DefaultHeaders)
+        // Logging calls made to API
+        install(CallLogging)
+        // Setting Routing
+        install(Routing)
+        // Format in which we want to sent the response
+        install(ContentNegotiation) {
+            gson {
+                setPrettyPrinting()
+            }
+        }
+
+
         configureRouting()
     }.start(wait = true)
 }
